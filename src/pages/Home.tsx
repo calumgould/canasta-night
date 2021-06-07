5aad9c0b0e56be5d3840fd08a5ac64583b847a88
Calum Gould <calumgould26@gmail.com>
2021-06-07 19:21:51 +0100

    linting

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,19 @@
-import React, { useEffect } from 'react'
-import { useHistory } from 'react-router-dom'
+import React from 'react'
 import Pressable from '../components/Pressable'
 
-const Home = () => {
-  const history = useHistory()
-
-  return (
-    <div>
-      <Pressable
-        onClick={async () => {
-          await fetch('http://localhost:3000/users')
-            .then((res) => res.json())
-            .then((users) => console.log(users))
-        }}
-        bordered
-      >
-        Get Users
-      </Pressable>
-    </div>
-  )
-}
+const Home = () => (
+  <div>
+    <Pressable
+      onClick={async () => {
+        await fetch('http://localhost:8000/users')
+          .then((res) => res.json())
+          .then((users) => console.log(users))
+      }}
+      bordered
+    >
+      Get Users
+    </Pressable>
+  </div>
+)
 
 export default Home
